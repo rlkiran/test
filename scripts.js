ae6d98f80c420e276581c38808d63d2b49af0086
rlkiran <50212698+rlkiran@users.noreply.github.com>
2020-09-20 21:52:20 +0530

    update final

--- scripts.js
+++ scripts.js
@@ -70,7 +70,7 @@ window.onload = function() {
 
 //AOS
 AOS.init({
-    offset: 200, // offset (in px) from the original trigger point
+    offset: 100, // offset (in px) from the original trigger point
     delay: 0, // values from 0 to 3000, with step 50ms
     duration: 1000 // values from 0 to 3000, with step 50ms
   });
---
+++ test.js
@@ -0,0 +1,6 @@
+//AOS
+AOS.init({
+    offset: 200, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 1000 // values from 0 to 3000, with step 50ms
+  });
